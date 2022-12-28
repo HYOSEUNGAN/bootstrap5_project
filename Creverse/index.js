38b623f9a31260de3744f2bf487a5b3fa20cb150
anhyoseung <ace.aaiden@gmail.com>
2022-12-28 16:01:49 +0900

    test

--- Creverse/index.js
+++ Creverse/index.js
@@ -1,1 +1,11 @@
 console.log("working");
+
+function darkMode() {
+  const checkbox = document.getElementById("flexSwitchCheckDefault");
+  const bodyElement = document.body;
+  if (checkbox.checked === false) {
+    bodyElement.classList.add("white-mode");
+  } else {
+    bodyElement.classList.remove("white-mode");
+  }
+}

2bff43ad7888b809ba2f4ba07ec0adc4ff6c28e7
anhyoseung <ace.aaiden@gmail.com>
2023-01-05 10:46:30 +0900

    check

---
+++ test.js
@@ -0,0 +1,29 @@
+//for
+// let arr = [3, 9, 4, 2, 7, 6];
+// let new_arr = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     new_arr.push(arr[i]);
+//   }
+// }
+
+// console.log(new_arr); //4,2,6
+
+//forEach()
+// arr.forEach((n) => {
+//   if (n % 2 === 0) {
+//     new_arr.push(n);
+//   }
+// });
+// console.log(new_arr); //4,2,6
+
+// let arr = ["foo", "hello", "diamond", "A"];
+
+// var arr2 = arr.map((v) => v.length);
+// console.log(arr2);
+
+//find
+// let arr = [4, 15, 377, 395, 400, 1024, 3000];
+// let arr2 = arr.find((n) => n % 5 === 0);
+// console.log(arr2);
